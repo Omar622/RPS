a51605f06481acbc89144d17a5fc98959da92ba0
Omar622 <omaramin622@gmail.com>
2023-02-06 16:11:19 +0200

    Make game consist of rounds with score
    * end round after either player or computer
      gets 5 points as score.
    * add score and status of the game in the UI.
    * make transform for buttons and some css.

--- script.js
+++ script.js
@@ -4,18 +4,23 @@ let getComputerChoice = () => {
   return validGames[randomIndex]
 }
 
+/*
+ In case player wins return 1
+ In case computer wins return -1
+ In case draw returns 0
+ */
 let playRound = (playerSelection, computerSelection) => {
   playerSelection = playerSelection.toLowerCase()
   computerSelection = computerSelection.toLowerCase()
 
   if (playerSelection === computerSelection) {
-    return `Draw! both played ${playerSelection}`
+    return 0;
   } else if ((playerSelection == 'rock' && computerSelection == 'paper')
     || (playerSelection == 'paper' && computerSelection == 'scissors')
     || (playerSelection == 'scissors' && computerSelection == 'rock')) {
-    return `You Lose! ${computerSelection} beats ${playerSelection}`
+    return -1;
   } else {
-    return `You Win! ${playerSelection} beats ${computerSelection}`
+    return 1;
   }
 }
 
@@ -41,9 +46,40 @@ let playRound = (playerSelection, computerSelection) => {
 
 // console.log(game())
 
+let playerScore = 0, computerScore = 0;
+const playerScoreElement = document.querySelector('.result .player');
+const computerScoreElement = document.querySelector('.result .computer');
+const statusElement = document.querySelector('.status');
+
 function playVsComputer(playerSelection) {
   let computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+  const result = playRound(playerSelection, computerSelection);
+  switch (result) {
+    case 1:
+      ++playerScore;
+      if(playerScore === 5){
+        statusElement.innerText = `WOW!! You Won This Round! You: ${playerScore}, computer: ${computerScore}`;
+        playerScoreElement.innerText = `You: ${playerScore = 0}`;
+        computerScoreElement.innerText = `computer: ${computerScore = 0}`;
+      }else{
+        statusElement.innerText = `You Win! ${playerSelection} beats ${computerSelection}`;
+        playerScoreElement.innerText = `You: ${playerScore}`;
+      }
+      break;
+    case -1:
+      ++computerScore;
+      if(computerScore === 5){
+        statusElement.innerText = `You Lost This Round! You: ${playerScore}, computer: ${computerScore}`;
+        playerScoreElement.innerText = `You: ${playerScore = 0}`;
+        computerScoreElement.innerText = `computer: ${computerScore = 0}`;
+      }else{
+        statusElement.innerText = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerScoreElement.innerText = `computer: ${computerScore}`;
+      }
+      break;
+    default:
+      statusElement.innerText = `Draw! ${playerSelection} and ${computerSelection}`;
+  }
 }
 
 const rockBtn = document.querySelector('.btn.rock');
